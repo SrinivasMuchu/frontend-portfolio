c6c5762ed597a70d303bd5a790318746d2a9932c
unknown <srinivasmuchu14@gmail.com>
2025-06-13 16:47:55 +0530

    gatag

--- src/App.js
+++ src/App.js
@@ -111,6 +111,16 @@ function App() {
 
           {/* Manifest file for Progressive Web App (PWA) */}
           <link rel="manifest" href="%PUBLIC_URL%/manifest.json" />
+          {/* Microsoft Clarity Tracking Script */}
+          <script type="text/javascript">
+            {`
+              (function(c,l,a,r,i,t,y){
+                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "rytiy1oxan");
+            `}
+          </script>
         </Helmet>
         <>
         {loading ? <Loading/> :    <Router>
